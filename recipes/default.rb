a45b18f591dfa9b97caaf6e86a9c3f189d77af85
three18ti <three18ti@gmail.com>
2013-12-10 16:23:04 -0700

    fixed test to determine if set node name is the same as ohai determined hostname, hostname can either be fqdn _or_ hostname.  removed set_fqdn

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,58 +24,47 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-service "networking" do
-    service_name "networking"
-    case node["platform"]
-    when "ubuntu"
-        if node["platform_version"].to_f >= 9.10
-            provider Chef::Provider::Service::Upstart
-        end
+unless node.name === node.hostname 
+    hostname = node.name
+
+    file '/etc/hostname' do
+        content "#{hostname}\n"
+        mode "0644"
+        notifies :reload, "ohai[reload]"
     end
-    action :restart
-end
 
-fqdn = node['set_fqdn']
+    execute "hostname #{hostname}" do
+        only_if { node['hostname'] != hostname }
+        notifies :reload, "ohai[reload]"
+        notifies :restart, "service[networking]"
+    end
 
-unless fqdn === node.hostname 
-    unless fqdn
-        log "Please set the set_fqdn attribute to desired hostname" do
-            level :warn
+    hostsfile_entry "localhost" do
+        ip_address "127.0.0.1"
+        hostname "localhost"
+        action :create
+    end
+
+    hostsfile_entry "set hostname" do
+        ip_address "127.0.1.1"
+        hostname hostname
+        action :create
+        notifies :reload, "ohai[reload]"
+    end
+
+    ohai "reload" do
+        action :nothing
+    end
+
+    service "networking" do
+        service_name "networking"
+        case node["platform"]
+        when "ubuntu"
+            if node["platform_version"].to_f >= 9.10
+                provider Chef::Provider::Service::Upstart
+            end
         end
-    else
-        fqdn = fqdn.sub('*', node.name)
-        fqdn =~ /^([^.]+)/
-        hostname = $1
-
-        file '/etc/hostname' do
-            content "#{hostname}\n"
-            mode "0644"
-            notifies :reload, "ohai[reload]"
-        end
-
-        execute "hostname #{hostname}" do
-            only_if { node['hostname'] != hostname }
-            notifies :reload, "ohai[reload]"
-            notifies :restart, "service[networking]"
-        end
-
-        hostsfile_entry "localhost" do
-            ip_address "127.0.0.1"
-            hostname "localhost"
-            action :create
-        end
-
-        hostsfile_entry "set hostname" do
-            ip_address "127.0.1.1"
-            hostname fqdn
-            aliases [ hostname ]
-            action :create
-            notifies :reload, "ohai[reload]"
-        end
-
-        ohai "reload" do
-            action :nothing
-        end
+        action :restart
     end
 else
     log "hostname already set, not doing anything" do
