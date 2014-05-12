3a357e1138beb14c483f05acbe66f242b9dbfb9c
three18ti <three18ti@gmail.com>
2014-05-12 16:33:48 -0600

    fixed foodcritic errors

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,28 +25,27 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
-unless node['name' === node['hostname'] 
-    hostname = node['name']
+unless node.name === node['hostname']
+    hostname = node.name
 
     hostname_file = file '/etc/hostname' do
         content "#{hostname}\n"
-        mode "0644"
+        mode '0644'
     end
-    
+
     exec_hostname = execute "hostname #{hostname}" do
         only_if { node['hostname'] != hostname }
 #        notifies :restart, "service[networking]"
     end
 
-    hostsfile_entry "localhost" do
-        ip_address "127.0.0.1"
-        hostname "localhost"
+    hostsfile_entry 'localhost' do
+        ip_address '127.0.0.1'
+        hostname 'localhost'
         action    :create_if_missing
     end
 
-    hostname_entry = hostsfile_entry "set hostname" do
-        ip_address "127.0.1.1"
+    hostname_entry = hostsfile_entry 'set hostname' do
+        ip_address '127.0.1.1'
         hostname hostname
         action :create
     end
@@ -59,8 +58,9 @@ unless node['name' === node['hostname']
         action :reload
     end
 
-    # should probably do this with a dhclient restart (since there's no need to restart networking when we're not on dhcp
-    execute "restart dhclient" do
+    # should probably do this with a dhclient restart 
+    # (since there's no need to restart networking when we're not on dhcp
+    execute 'restart dhclient' do
         command "echo 'restarting dhclient'; dhclient -r -v; dhclient -v;"
         action :run
     end
@@ -71,21 +71,21 @@ unless node['name' === node['hostname']
 #            service_name "networking"
 #            if node["platform_version"].to_f >= 9.10 && node["platform_version"].to_f < 14.04
 #                provider Chef::Provider::Service::Upstart
-#            else 
+#            else
                 # dummy else, since 14.04 no longer supports networking "restart"
 #                service_name "networking"
 #            end
 #        when "fedora"
 #            service_name "network"
 #            action :restart
-#        else 
+#        else
 #            service_name "network"
 #            action :restart
 #        end
 #    end
 else
-    log "hostname set" do
-        message "hostname matches node name, not doing anything"
+    log 'hostname set' do
+        message 'hostname matches node name, not doing anything'
         level :warn
     end
     return
