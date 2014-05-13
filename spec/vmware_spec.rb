04fa8ebb6ec2bc2f578203ff1993a45eed587626
three18ti <three18ti@gmail.com>
2014-05-13 00:07:36 -0600

    attempt to fix tests after cookbook rename

--- spec/vmware_spec.rb
+++ spec/vmware_spec.rb
@@ -1,7 +1,7 @@
 require 'chefspec'
 
-describe 'hostname::vmware' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'hostname::vmware' }
+describe 'set_hostname::vmware' do
+  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'set_hostname::vmware' }
   it 'should do something' do
     pending 'Your recipe examples go here.'
   end
